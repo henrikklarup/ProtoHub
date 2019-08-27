e23f8db3b695ec659fcc8d349b5169cc14f585fa
henrikklarup <henrikklarup@gmail.com>
2019-08-27 20:57:48 +0200

    Begin running commands

--- ProtoHub/ProtoHubImpl.cs
+++ ProtoHub/ProtoHubImpl.cs
@@ -1,11 +1,24 @@
+using System;
+using System.Diagnostics;
 using System.Threading.Tasks;
 using ProtoHub;
 
 public class ProtoHubImpl : ProtoHubService.ProtoHubServiceBase
 {
-    public override Task<UploadSpecResponse> UploadSpec(UploadSpecRequest request, Grpc.Core.ServerCallContext context)
+    public override async Task<UploadSpecResponse> UploadSpec(UploadSpecRequest request, Grpc.Core.ServerCallContext context)
     {
-        return Task.FromResult(new UploadSpecResponse {
+        Process cmd = new Process();
+        cmd.StartInfo.FileName = "prototool";
+        cmd.StartInfo.RedirectStandardOutput = true;
+        cmd.StartInfo.Arguments = "files ../Protos";
+        cmd.StartInfo.CreateNoWindow = true;
+        cmd.StartInfo.UseShellExecute = false;
+
+        cmd.Start();
+        cmd.WaitForExit();
+        Console.WriteLine(cmd.StandardOutput.ReadToEnd());
+
+        return await Task.FromResult(new UploadSpecResponse {
             Status = new Status {
                 Code = 1,
                 Message = "Complete",
